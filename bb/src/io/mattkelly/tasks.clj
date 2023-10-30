8e391a17358f10db1a60fd53c5568f2e227aae18
Matt Kelly <matthew.ryan.kelly@gmail.com>
2023-10-30 13:59:17 -0400

    Init new biffweb app

---
+++ bb/src/io/mattkelly/tasks.clj
@@ -0,0 +1,6 @@
+(ns io.mattkelly.tasks)
+
+(defn hello
+  "An example of a custom task."
+  []
+  (println "Hello there."))
---
+++ src/io/mattkelly/settings.clj
@@ -0,0 +1,3 @@
+(ns io.mattkelly.settings)
+
+(def app-name "My Application")

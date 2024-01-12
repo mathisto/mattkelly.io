de42979311f4d99c33d84c75fdb6be553b0b7537
Matt Kelly <matthew.ryan.kelly@gmail.com>
2024-01-12 15:18:44 -0500

    Fix configs with server specific info

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server '195.35.36.247', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax

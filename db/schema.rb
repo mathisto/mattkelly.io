6b202b3d0be7b0fa08b96895e123dd4fb0a48491
Matt Kelly <matthew.ryan.kelly@gmail.com>
2024-01-28 09:35:42 -0600

    Migrate utterances table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_28_153130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "utterances", force: :cascade do |t|
+    t.string "text"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

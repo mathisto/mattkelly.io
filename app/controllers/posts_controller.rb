f8e5c7c16b1089f89c573896d481c145ca8f7e8b
Matt Kelly <8226466+mathisto@users.noreply.github.com>
2025-03-12 06:07:44 -0400

    Fix blog page rendering

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,15 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+  rescue Errno::ENOENT
+    # If the blog directory doesn't exist, show an empty array
+    @posts = []
   end
 
   def show
     @post = Post.find(params[:id])
+  rescue Errno::ENOENT
+    # If the blog directory or file doesn't exist, redirect to index
+    redirect_to posts_path, alert: "Post not found"
   end
 end 

553f062c44a5c73aaaa9b5346bdd38f0841881bb
Matt Kelly <8226466+mathisto@users.noreply.github.com>
2025-03-13 10:45:37 -0400

    Restore active model

--- config/application.rb
+++ config/application.rb
@@ -4,35 +4,29 @@ require_relative "boot"
 require "rails"
 
 # Pick the frameworks you want:
-require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
 require "rails/test_unit/railtie"
-require "sprockets/railtie"
+require "active_model/railtie"
+require "propshaft"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production
+# Require the gems listed in Gemfile
 Bundler.require(*Rails.groups)
 
 module MattkellyIo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks templates))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "UTC"
   end
 end

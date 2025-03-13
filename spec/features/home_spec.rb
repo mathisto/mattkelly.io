1efe1b345253828d17ef9964e58cdcaf0dc4a75b
Matt Kelly <8226466+mathisto@users.noreply.github.com>
2025-03-13 13:22:18 -0400

    Update home page feature spec to use have_content matcher
    This commit modifies the home page feature spec to replace the have_selector matcher with have_content for checking the presence of the welcome message. This change improves the readability and intent of the test.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Home page', type: :feature do
   it 'displays the welcome message' do
     visit root_path
-    expect(page).to have_selector('h1', text: 'Matt Kelly')
+    expect(page).to have_content('Matt Kelly')
   end
 end 

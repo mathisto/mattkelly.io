eff40dbbc4ef790f87be3819c717db3d0a08a741
Matt Kelly <matthew.ryan.kelly@gmail.com>
2025-11-30 15:02:31 -0500

    Add Tailscale domain to host authorization

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Allow requests from Tailscale domain
+  config.hosts << "hopper.sphinx-ruler.ts.net"
 end
